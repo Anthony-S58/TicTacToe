6c11b60f1c35d9da83487b9112a048113b9802ff
Anthony-S58 <tonysim01@hotmail.com>
2021-09-03 11:13:19 +0200

    test1

---
+++ app.js
@@ -0,0 +1,16 @@
+
+    let cadre1 = document.getElementById('cadre1');
+    let cadre2 = document.getElementById('cadre2');
+    let cadre3 = document.getElementById('cadre3');
+    let cadre4 = document.getElementById('cadre4');
+    let cadre5 = document.getElementById('cadre5');
+    let cadre6 = document.getElementById('cadre6');
+    let cadre7 = document.getElementById('cadre7');
+    let cadre8 = document.getElementById('cadre8');
+    let cadre9 = document.getElementById('cadre9');
+
+    cadre1.addEventListener('click', function(){
+        cadre1.textContent="X";
+    })
+
+    
